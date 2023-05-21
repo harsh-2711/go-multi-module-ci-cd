dffbd10671a6b429c9def0a13472021de912e3c7
harsh-2711 <harshaudi27@gmail.com>
2023-05-21 23:29:58 +0530

    refactor: github workflow file name

--- packages/even/even.go
+++ packages/even/even.go
@@ -3,6 +3,6 @@ package even
 import "fmt"
 
 func IsEven(i int) bool {
-	fmt.Println("function called: IsEven") // to test auto dependency management of Golang
+	fmt.Println("func called: IsEven") // to test auto dependency management of Golang
 	return i%2 == 0
 }
