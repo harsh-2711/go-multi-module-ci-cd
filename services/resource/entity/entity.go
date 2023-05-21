c14425ec456a34a70d70a301accfc38f3bd76166
harsh-2711 <harshaudi27@gmail.com>
2023-05-21 23:22:01 +0530

    chore: add github action workflow to auto tag packages

--- services/resource/entity/entity.go
+++ services/resource/entity/entity.go
@@ -1,7 +1,11 @@
 package entity
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 func GetUserEntity(userId int) int {
+	fmt.Println("function called: GetUserEntity") // to test auto dependency management of Golang
 	return int(math.Pow(float64(userId), 2))
 }
