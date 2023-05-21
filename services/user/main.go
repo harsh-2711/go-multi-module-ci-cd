782e786535e9c91c2a180d998bcb792abdaa6176
harsh-2711 <harshaudi27@gmail.com>
2023-05-21 23:08:59 +0530

    fix: dependencies in user service

---
+++ services/user/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/harsh-2711/go-multi-module-ci-cd/packages/even"
+	"github.com/harsh-2711/go-multi-module-ci-cd/services/resource/entity"
+)
+
+func main() {
+	fmt.Println("input userId:")
+	var userId int
+	_, err := fmt.Scanln(&userId)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if even.IsEven(userId) {
+		fmt.Println("even")
+	} else {
+		fmt.Println("odd")
+	}
+
+	userEntityId := entity.GetUserEntity(userId)
+	fmt.Println("userEntityId:", userEntityId)
+}
