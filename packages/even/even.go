3b87bbcea8175efe107d50093d990ee0da1dc48d
harsh-2711 <harshaudi27@gmail.com>
2023-05-31 15:56:31 +0530

    fix: add write permission + reformat detect changes step

--- packages/even/even.go
+++ packages/even/even.go
@@ -2,9 +2,7 @@ package even
 
 import "fmt"
 
-func IsEven(i int, shouldLog bool) bool {
-	if shouldLog {
-		fmt.Println("IsEven function called") // to test auto dependency management of Golang
-	}
+func IsEven(i int) bool {
+	fmt.Println("IsEven function called") // to test auto dependency management of Golang
 	return i%2 == 0
 }
