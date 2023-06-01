9095ba8c2ef1c909107397ef7852d0ff37dbb8fc
harsh-2711 <harshaudi27@gmail.com>
2023-06-01 13:14:32 +0530

    fix: git diff operation in workflow

--- a.sh
+++
@@ -1,8 +0,0 @@
-echo "::group::Detecting changed packages"
-CHANGED_PACKAGES=$(git diff --name-only ${{ github.event.before }}..${{ github.sha }} | grep '^packages/' | cut -d/ -f2 | sort -u || '')
-echo "Changed packages: $CHANGED_PACKAGES"
-echo "::endgroup::"
-if [[ -z "$CHANGED_PACKAGES" ]]; then
-	echo "No changes detected in packages, exiting"
-	exit 0
-fi
