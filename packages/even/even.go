80b18440d8d1a81e0005eaae5e9b5dd81329bdf8
harsh-2711 <harshaudi27@gmail.com>
2023-06-01 12:56:15 +0530

    chore: create packages matrix step

--- packages/even/even.go
+++ packages/even/even.go
@@ -2,7 +2,9 @@ package even
 
 import "fmt"
 
-func IsEven(i int) bool {
-	fmt.Println("IsEven function called") // to test auto dependency management of Golang
+func IsEven(i int, shouldLog bool) bool {
+	if shouldLog {
+		fmt.Println("IsEven function called") // to test auto dependency management of Golang
+	}
 	return i%2 == 0
 }
